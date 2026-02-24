a5643408ec47cb716c02008e08146d84b4c91a41
Kavindya R.S <165242205+sewmini12@users.noreply.github.com>
2026-02-24 16:17:48 +0530

    make pages structure

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    //repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
@@ -27,7 +27,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
